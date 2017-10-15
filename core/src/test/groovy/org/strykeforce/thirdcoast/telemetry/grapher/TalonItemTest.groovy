c8bfd62cbb79aa9131d307a06a6b933c83d159c1
Jeff Hutchison <jeff@jeffhutchison.com>
2017-10-15 19:34:19 -0400

    Add TalonItem tests

---
+++ core/src/test/groovy/org/strykeforce/thirdcoast/telemetry/grapher/TalonItemTest.groovy
@@ -0,0 +1,34 @@
+package org.strykeforce.thirdcoast.telemetry.grapher
+
+import com.ctre.CANTalon
+import spock.lang.Specification
+
+class TalonItemTest extends Specification {
+    // TODO: add rest of measurements
+    def "MeasurementFor"() {
+        given:
+        def talon = Stub(CANTalon)
+        talon.getSetpoint() >> 0.2767
+        talon.getOutputCurrent() >> 2.767
+        talon.getOutputVoltage() >> 27.67
+        Item item = new TalonItem(talon)
+
+        when:
+        def d = item.measurementFor(Measure.SETPOINT)
+
+        then:
+        d.getAsDouble() == 0.2767
+
+        when:
+        d = item.measurementFor(Measure.OUTPUT_CURRENT)
+
+        then:
+        d.getAsDouble() == 2.767
+
+        when:
+        d = item.measurementFor(Measure.OUTPUT_VOLTAGE)
+
+        then:
+        d.getAsDouble() == 27.67
+    }
+}
