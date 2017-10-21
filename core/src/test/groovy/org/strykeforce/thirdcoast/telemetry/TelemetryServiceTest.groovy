4cd4a3d300a76df718696cd6611fbf9b3083847e
Jeff Hutchison <jeff@jeffhutchison.com>
2017-10-21 08:07:19 -0400

    Relocate and add tests

--- core/src/test/groovy/org/strykeforce/thirdcoast/telemetry/TelemetryServiceTest.groovy
+++ core/src/test/groovy/org/strykeforce/thirdcoast/telemetry/TelemetryServiceTest.groovy
@@ -1,7 +1,37 @@
 package org.strykeforce.thirdcoast.telemetry
 
+import com.ctre.CANTalon
+import org.strykeforce.thirdcoast.talon.StatusFrameRate
 import spock.lang.Specification
 
 class TelemetryServiceTest extends Specification {
+
+    def "sets status frame rates for given Talon"() {
+        given:
+        def talon = Mock(CANTalon)
+        talon.getDeviceID() >>> [1, 3, 5, 4]
+        def target = Mock(CANTalon)
+        target.getDeviceID() >> 4
+        def telemetry = new TelemetryService()
+        def rates = StatusFrameRate.builder().general(2767).build()
+
+        when:
+        telemetry.register(talon)  // 1
+        telemetry.register(talon)  // 3
+        telemetry.register(target) // 4
+        telemetry.register(talon)  // 5
+        telemetry.register(talon)  // 4
+
+        then:
+        4 * talon.setStatusFrameRateMs(CANTalon.StatusFrameRate.General, 10)
+        1 * target.setStatusFrameRateMs(CANTalon.StatusFrameRate.General, 10)
+
+        when:
+        telemetry.configureStatusFrameRates(4, rates)
+
+        then:
+        1 * target.setStatusFrameRateMs(CANTalon.StatusFrameRate.General, 2767)
+        0 * talon.setStatusFrameRateMs(CANTalon.StatusFrameRate.General, _)
+    }
 
 }
--- core/src/test/groovy/org/strykeforce/thirdcoast/telemetry/item/TalonItemTest.groovy
+++ core/src/test/groovy/org/strykeforce/thirdcoast/telemetry/item/TalonItemTest.groovy
@@ -1,8 +1,6 @@
 package org.strykeforce.thirdcoast.telemetry.item
 
 import com.ctre.CANTalon
-import org.strykeforce.thirdcoast.talon.StatusFrameRate
-import org.strykeforce.thirdcoast.telemetry.TelemetryService
 import org.strykeforce.thirdcoast.telemetry.grapher.Measure
 import spock.lang.Specification
 
@@ -35,32 +33,4 @@ class TalonItemTest extends Specification {
         d.getAsDouble() == 27.67
     }
 
-    def "sets status frame rates for given Talon"() {
-        given:
-        def talon = Mock(CANTalon)
-        talon.getDeviceID() >>> [1, 3, 5]
-        def target = Mock(CANTalon)
-        target.getDeviceID() >> 4
-        def telemetry = new TelemetryService()
-        def rates = StatusFrameRate.builder().general(2767).build()
-
-        when:
-        telemetry.register(talon)
-        telemetry.register(talon)
-        telemetry.register(target)
-        telemetry.register(talon)
-
-        then:
-        3 * talon.setStatusFrameRateMs(CANTalon.StatusFrameRate.General, 10)
-        1 * target.setStatusFrameRateMs(CANTalon.StatusFrameRate.General, 10)
-
-        when:
-        telemetry.configureStatusFrameRates(4, rates)
-
-        then:
-        1 * target.setStatusFrameRateMs(CANTalon.StatusFrameRate.General, 2767)
-        0 * talon.setStatusFrameRateMs(CANTalon.StatusFrameRate.General, _)
-
-
-    }
 }
