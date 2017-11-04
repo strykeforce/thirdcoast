a32d1265061b1ec7f9a9c16ba7dd7656c4bef25f
Jeff Hutchison <jeff@jeffhutchison.com>
2017-11-04 19:57:30 -0400

    Add ForwardSoftLimitCommand test

--- tct/src/main/java/org/strykeforce/thirdcoast/telemetry/tct/talon/config/lim/ForwardSoftLimitCommand.java
+++ tct/src/main/java/org/strykeforce/thirdcoast/telemetry/tct/talon/config/lim/ForwardSoftLimitCommand.java
@@ -7,16 +7,22 @@ import org.strykeforce.thirdcoast.telemetry.tct.talon.TalonSet;
 import org.strykeforce.thirdcoast.telemetry.tct.talon.config.AbstractDoubleConfigCommand;
 
 public class ForwardSoftLimitCommand extends AbstractDoubleConfigCommand {
+
   public final static String NAME = "Forward Soft Limit";
 
   @Inject
-  public ForwardSoftLimitCommand(TalonSet talonSet, LineReader reader) {
+  public ForwardSoftLimitCommand(LineReader reader, TalonSet talonSet) {
     super(NAME, reader, talonSet);
   }
 
   @Override
   protected void config(CANTalon talon, double value) {
+    if (value == 0) {
+      talon.enableForwardSoftLimit(false);
+      return;
+    }
     talon.setForwardSoftLimit(value);
+    talon.enableForwardSoftLimit(true);
   }
 
 }
---
+++ tct/src/test/groovy/org/strykeforce/thirdcoast/telemetry/tct/talon/config/lim/ForwardSoftLimitCommandTest.groovy
@@ -0,0 +1,48 @@
+package org.strykeforce.thirdcoast.telemetry.tct.talon.config.lim
+
+import org.strykeforce.thirdcoast.telemetry.tct.Command
+import org.strykeforce.thirdcoast.telemetry.tct.talon.config.AbstractTalonConfigCommandTest
+
+class ForwardSoftLimitCommandTest extends AbstractTalonConfigCommandTest {
+
+    Command command
+
+    @Override
+    void setup() {
+        command = new ForwardSoftLimitCommand(reader, talonSet)
+    }
+
+    def "nothing input"() {
+        when:
+        command.perform()
+
+        then:
+        1 * reader.readLine(_) >> ""
+        0 * talon._
+    }
+
+    def "handles input"() {
+        when:
+        command.perform()
+
+        then:
+        1 * reader.readLine(_) >>> ["27.67"]
+
+        1 * talon.setForwardSoftLimit(27.67) // 27.67
+        1 * talon.enableForwardSoftLimit(true)
+        1 * talon.getDescription()
+        0 * talon._
+    }
+
+    def "handles zero as disable"() {
+        when:
+        command.perform()
+
+        then:
+        1 * reader.readLine(_) >>> ["0"]
+
+        1 * talon.enableForwardSoftLimit(false)
+        1 * talon.getDescription()
+        0 * talon._
+    }
+}
