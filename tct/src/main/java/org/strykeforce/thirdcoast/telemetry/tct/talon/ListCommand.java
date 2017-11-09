27e6b425ca7508dfd2ba7187252110b28e19613a
Jeff Hutchison <jeff@jeffhutchison.com>
2017-11-09 10:13:14 -0500

    List talon details

--- tct/src/main/java/org/strykeforce/thirdcoast/telemetry/tct/talon/ListCommand.java
+++ tct/src/main/java/org/strykeforce/thirdcoast/telemetry/tct/talon/ListCommand.java
@@ -1,11 +1,16 @@
 package org.strykeforce.thirdcoast.telemetry.tct.talon;
 
 import com.ctre.CANTalon;
+import com.ctre.CANTalon.TalonControlMode;
+import com.ctre.CANTalon.VelocityMeasurementPeriod;
+import java.util.Formatter;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import javax.annotation.Nullable;
 import javax.annotation.ParametersAreNonnullByDefault;
 import javax.inject.Inject;
 import org.jline.reader.LineReader;
-import org.jline.utils.AttributedStringBuilder;
-import org.jline.utils.AttributedStyle;
 import org.strykeforce.thirdcoast.telemetry.tct.AbstractCommand;
 import org.strykeforce.thirdcoast.telemetry.tct.di.ModeScoped;
 import org.strykeforce.thirdcoast.telemetry.tct.talon.di.TalonMenuModule;
@@ -18,27 +23,135 @@ import org.strykeforce.thirdcoast.telemetry.tct.talon.di.TalonMenuModule;
 public class ListCommand extends AbstractCommand {
 
   public final static String NAME = "List Talons";
+  private final static String FORMAT_DESCRIPTION = "%12s";
+  private final static String FORMAT_DOUBLE = "%12.3f";
+  private final static String FORMAT_INTEGER = "%12d";
+  private final static String FORMAT_STRING = "%12s";
+
   private final TalonSet talonSet;
 
   @Inject
-  ListCommand(TalonSet talonSet, LineReader reader) {
+  ListCommand(LineReader reader, TalonSet talonSet) {
     super(NAME, TalonMenuModule.MENU_ORDER.indexOf(NAME), reader);
     this.talonSet = talonSet;
   }
 
   @Override
   public void perform() {
-    terminal.writer().printf(header());
-    for (CANTalon talon : talonSet.selected()) {
-      terminal.writer().printf("%d: %15s %8s %2s%n", talon.getDeviceID(), talon.getDescription(),
-          talon.getControlMode(), talonSet.selected().contains(talon) ? "Y" : "");
-    }
+    Set<CANTalon> talons = talonSet.selected();
+    terminal.writer().print(header());
+    stringLine("Mode:", talons.stream().map(CANTalon::getControlMode).map(TalonControlMode::name)
+        .collect(Collectors.toList()));
+    doubleLine("Setpoint:",
+        talons.stream().map(CANTalon::getSetpoint).collect(Collectors.toList()));
+    doubleLine("P:", talons.stream().map(CANTalon::getP).collect(Collectors.toList()));
+    doubleLine("I:", talons.stream().map(CANTalon::getI).collect(Collectors.toList()));
+    doubleLine("D:", talons.stream().map(CANTalon::getD).collect(Collectors.toList()));
+    doubleLine("F:", talons.stream().map(CANTalon::getF).collect(Collectors.toList()));
+    doubleLine("I-zone:", talons.stream().map(CANTalon::getIZone).collect(Collectors.toList()));
+
+    doubleLine("Position:",
+        talons.stream().map(CANTalon::getPosition).collect(Collectors.toList()));
+    intLine("Abs Enc:",
+        talons.stream().map(CANTalon::getPulseWidthPosition).map(pos -> pos & 0xFFF)
+            .collect(Collectors.toList()));
+
+    intLine("Fwd Soft:",
+        talons.stream().map(CANTalon::getForwardSoftLimit).collect(Collectors.toList()));
+    intLine("Rev Soft:",
+        talons.stream().map(CANTalon::getReverseSoftLimit).collect(Collectors.toList()));
+
+    booleanLine("Fwd LS Clsd:",
+        talons.stream().map(CANTalon::isFwdLimitSwitchClosed).collect(Collectors.toList()));
+    booleanLine("Rev LS Clsd:",
+        talons.stream().map(CANTalon::isRevLimitSwitchClosed).collect(Collectors.toList()));
+
+    intLine("Analog:", talons.stream().map(CANTalon::getAnalogInRaw).collect(Collectors.toList()));
+
+    intLine("VM Window:",
+        talons.stream().map(CANTalon::GetVelocityMeasurementWindow).collect(Collectors.toList()));
+    intLine("VM Period:",
+        talons.stream().map(CANTalon::GetVelocityMeasurementPeriod).map(ListCommand::intValue)
+            .collect(Collectors.toList()));
     terminal.writer().println();
   }
 
-  private String header() {
-    String header = String.format("ID %15s %8s Sel.%n", "Talon", "Mode");
-    return new AttributedStringBuilder().style(AttributedStyle.BOLD).append(header).toAnsi();
+  private void stringLine(@SuppressWarnings("SameParameterValue") String description,
+      List<String> values) {
+    StringBuilder sb = new StringBuilder();
+    Formatter formatter = new Formatter(sb);
+    sb.append(bold(String.format(FORMAT_DESCRIPTION, description)));
+    for (String val : values) {
+      formatter.format(FORMAT_STRING, val);
+    }
+    terminal.writer().println(sb.toString());
   }
 
+  private void booleanLine(String description, List<Boolean> values) {
+    StringBuilder sb = new StringBuilder();
+    Formatter formatter = new Formatter(sb);
+    sb.append(bold(String.format(FORMAT_DESCRIPTION, description)));
+    for (Boolean val : values) {
+      formatter.format(FORMAT_STRING, val ? "YES" : "NO");
+    }
+    terminal.writer().println(sb.toString());
+  }
+
+  private void intLine(String description, List<Integer> values) {
+    StringBuilder sb = new StringBuilder();
+    Formatter formatter = new Formatter(sb);
+    sb.append(bold(String.format(FORMAT_DESCRIPTION, description)));
+    for (Integer val : values) {
+      formatter.format(FORMAT_INTEGER, val);
+    }
+    terminal.writer().println(sb.toString());
+  }
+
+  private void doubleLine(String description, List<Double> values) {
+    StringBuilder sb = new StringBuilder();
+    Formatter formatter = new Formatter(sb);
+    sb.append(bold(String.format(FORMAT_DESCRIPTION, description)));
+    for (Double val : values) {
+      formatter.format(FORMAT_DOUBLE, val);
+    }
+    terminal.writer().println(sb.toString());
+  }
+
+  private String header() {
+    // desc(12) value(10) * 6
+    StringBuilder sb = new StringBuilder();
+    Formatter formatter = new Formatter(sb);
+    formatter.format(FORMAT_DESCRIPTION, "Talon:");
+    for (CANTalon talon : talonSet.selected()) {
+      formatter.format(FORMAT_INTEGER, talon.getDeviceID());
+    }
+    sb.append("\n");
+    return bold(sb.toString());
+  }
+
+  private static int intValue(@Nullable VelocityMeasurementPeriod period) {
+    if (period == null) {
+      return 0;
+    }
+    switch (period) {
+      case Period_1Ms:
+        return 1;
+      case Period_2Ms:
+        return 2;
+      case Period_5Ms:
+        return 5;
+      case Period_10Ms:
+        return 10;
+      case Period_20Ms:
+        return 20;
+      case Period_25Ms:
+        return 25;
+      case Period_50Ms:
+        return 50;
+      case Period_100Ms:
+        return 100;
+      default:
+        return 0;
+    }
+  }
 }
