bf09a409c6ee3ad6e612ceaa64d14b4100190041
Jeff Hutchison <jeff@jeffhutchison.com>
2017-11-16 15:46:37 -0500

    Add pulse run feature

--- tct/src/main/java/org/strykeforce/thirdcoast/telemetry/tct/talon/RunCommand.java
+++ tct/src/main/java/org/strykeforce/thirdcoast/telemetry/tct/talon/RunCommand.java
@@ -1,6 +1,9 @@
 package org.strykeforce.thirdcoast.telemetry.tct.talon;
 
 import com.ctre.CANTalon;
+import edu.wpi.first.wpilibj.Timer;
+import java.util.Arrays;
+import java.util.List;
 import javax.annotation.ParametersAreNonnullByDefault;
 import javax.inject.Inject;
 import org.jline.reader.EndOfFileException;
@@ -27,7 +30,8 @@ public class RunCommand extends AbstractCommand {
       terminal.writer().println(Messages.NO_TALONS);
       return;
     }
-    terminal.writer().println(Messages.bold("Enter motor setpoint, press <enter> to go back"));
+    terminal.writer().println(Messages.bold("enter motor setpoint, press <enter> to go back"));
+    terminal.writer().println(Messages.bold("optionally, enter setpoint, duration to pulse"));
     while (true) {
       String line;
       try {
@@ -40,16 +44,46 @@ public class RunCommand extends AbstractCommand {
         return;
       }
       double setpoint;
+      double duration;
+      List<String> entries = Arrays.asList(line.split(","));
       try {
-        setpoint = Double.valueOf(line);
+        if (entries.size() > 0) {
+          setpoint = Double.valueOf(entries.get(0));
+        } else {
+          help();
+          continue;
+        }
+        duration = entries.size() > 1 ? Double.valueOf(entries.get(1)) : -1;
       } catch (NumberFormatException nfe) {
-        terminal.writer().println(Messages.boldRed("please enter a number"));
+        help();
         continue;
       }
-      terminal.writer().print(Messages.bold(String.format("setting talons to %.2f%n", setpoint)));
-      for (CANTalon talon : talonSet.selected()) {
-        talon.set(setpoint);
+
+      if (duration > 0) {
+        terminal.writer().print(Messages
+            .bold(String.format("setting talons to %.2f for %.2f sec%n", setpoint, duration)));
+        terminal.flush();
+        double old = talonSet.selected().stream().findFirst().map(CANTalon::get).orElse(0.0);
+        setTalons(setpoint);
+        Timer.delay(duration);
+        setTalons(old);
+      } else {
+        terminal.writer().print(Messages.bold(String.format("setting talons to %.2f%n", setpoint)));
+        terminal.flush();
+        setTalons(setpoint);
       }
     }
   }
+
+  protected void setTalons(double setpoint) {
+    for (CANTalon talon : talonSet.selected()) {
+      talon.set(setpoint);
+    }
+  }
+
+  protected void help() {
+    terminal.writer()
+        .println(Messages.boldRed("please enter a number or two numbers separated by a commma"));
+  }
+
 }
