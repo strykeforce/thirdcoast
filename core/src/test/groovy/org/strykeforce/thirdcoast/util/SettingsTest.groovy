35cd9379de95f983dd8e98131ada588c4d6157d1
Jeff Hutchison <jeff@jeffhutchison.com>
2018-02-13 10:24:17 -0500

    Use URL instead of File for config
    This allows passing in a resource in the JAR file for configuration.

--- core/src/test/groovy/org/strykeforce/thirdcoast/util/SettingsTest.groovy
+++ core/src/test/groovy/org/strykeforce/thirdcoast/util/SettingsTest.groovy
@@ -1,18 +1,33 @@
 package org.strykeforce.thirdcoast.util
 
 import com.ctre.phoenix.motorcontrol.ControlMode
+import com.moandjiezana.toml.Toml
 import spock.lang.Specification
 
 class SettingsTest extends Specification {
 
+    void matchesDefaultWheel(Toml wheel) {
+        assert wheel.getLong("ticksPerRevolution") == 4096L
+        assert wheel.getLong("driveSetpointMax") == 0L
+        assert wheel.getString("azimuthControlMode") == ControlMode.MotionMagic.name()
+        assert wheel.getString("driveOpenLoopControlMode") == ControlMode.PercentOutput.name()
+        assert wheel.getString("driveClosedLoopControlMode") == ControlMode.Velocity.name()
+    }
+
     def "reads table defaults"() {
         when:
         def settings = new Settings()
-        def toml = settings.getTable("THIRDCOAST.SWERVE")
+        def swerve = settings.getTable("THIRDCOAST.SWERVE")
 
         then:
-        toml.getDouble("length") == 1.0
-        toml.getDouble("width") == 1.0
+        swerve.getDouble("length") == 1.0
+        swerve.getDouble("width") == 1.0
+
+        when:
+        def wheel = settings.getTable("THIRDCOAST.WHEEL")
+
+        then:
+        matchesDefaultWheel(wheel)
     }
 
     def "overrides table defaults"() {
@@ -26,12 +41,47 @@ class SettingsTest extends Specification {
 
         then:
         with(wheel) {
-            ticksPerRevolution == 2767
-            driveSetpointMax == 0
-            azimuthControlMode == ControlMode.MotionMagic.name()
-            driveOpenLoopControlMode == ControlMode.PercentOutput.name()
-            driveClosedLoopControlMode == ControlMode.Disabled.name()
+            getLong("ticksPerRevolution") == 2767
+            getLong("driveSetpointMax") == 0
+            getString("azimuthControlMode") == ControlMode.MotionMagic.name()
+            getString("driveOpenLoopControlMode") == ControlMode.PercentOutput.name()
+            getString("driveClosedLoopControlMode") == ControlMode.Disabled.name()
+        }
+    }
+
+    def "reads settings from resource URL with merged defaults"() {
+        given:
+        def url = this.class.getResource("/testdata/settings/test.toml")
+
+        when:
+        def settings = new Settings(url)
+        def wheel = settings.getTable("THIRDCOAST.WHEEL")
+
+        then:
+        with(wheel) {
+            getLong("ticksPerRevolution") == 4096
+            getLong("driveSetpointMax") == 2767
+            getString("azimuthControlMode") == ControlMode.Disabled.name()
+            getString("driveOpenLoopControlMode") == ControlMode.PercentOutput.name()
+            getString("driveClosedLoopControlMode") == ControlMode.Velocity.name()
         }
 
+        when:
+        def talons = settings.getTable("THIRDCOAST.TALONS")
+
+        then:
+        talons.timeout == 2767
+    }
+
+    def "reads default setting from missing URL"() {
+        given:
+        def url = this.class.getResource("/testdata/settings/missing.toml")
+
+        when:
+        def settings = new Settings(url as URL)
+        def wheel = settings.getTable("THIRDCOAST.WHEEL")
+
+        then:
+        matchesDefaultWheel(wheel)
     }
 }
