76e2268fe993228e824de8bf0a93b59426f235b1
Jeff Hutchison <jeff@jeffhutchison.com>
2020-01-20 18:44:20 -0500

    Fix equals check recursive call

--- src/main/kotlin/org/strykeforce/thirdcoast/telemetry/item/TalonFXItem.kt
+++ src/main/kotlin/org/strykeforce/thirdcoast/telemetry/item/TalonFXItem.kt
@@ -87,7 +87,7 @@ class TalonFXItem @JvmOverloads constructor(
     }
 
     override fun equals(other: Any?): Boolean {
-        if (this == other) return true
+        if (this === other) return true
         if (javaClass != other?.javaClass) return false
         other as TalonFXItem
         if (deviceId != other.deviceId) return false
