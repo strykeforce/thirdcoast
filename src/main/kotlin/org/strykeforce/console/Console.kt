d673f1f50e9e421fc951060feb583fa0996477e9
Jeff Hutchison <jeff@j3ff.io>
2021-05-27 08:13:35 -0400

    Reorganize Console classes

--- src/main/kotlin/org/strykeforce/console/Console.kt
+++ src/main/kotlin/org/strykeforce/console/Console.kt
@@ -1,7 +1,5 @@
 package org.strykeforce.console
 
-import edu.wpi.first.wpilibj2.command.button.Button
-
 interface Console {
     enum class Switch { CENTER, NORTH, SOUTH, EAST, WEST }
 
@@ -9,5 +7,32 @@ interface Console {
     fun periodic()
     fun writeString(string: String, x: Int, y: Int, on: Boolean = true)
     fun writeStringCentered(string: String, y: Int, on: Boolean = true)
-    fun getSwitch(switch: Switch) : Boolean
+    fun getSwitch(switch: Switch): Boolean
+}
+
+@ExperimentalStdlibApi
+class ConsoleImpl : Console {
+    val display = SSD1306()
+    val expander = MCP23008()
+
+    override fun clear() = display.clear()
+
+    override fun periodic() = display.periodic()
+
+    override fun writeString(string: String, x: Int, y: Int, on: Boolean) =
+        display.drawString(string, Font.FONT_5X8, x, y, on)
+
+    override fun writeStringCentered(string: String, y: Int, on: Boolean) =
+        display.drawStringCentered(string, Font.FONT_5X8, y, on)
+
+    override fun getSwitch(switch: Console.Switch) = expander.getSwitch(switch)
+}
+
+class DummyConsole : Console {
+
+    override fun clear() {}
+    override fun periodic() {}
+    override fun writeString(string: String, x: Int, y: Int, on: Boolean) {}
+    override fun writeStringCentered(string: String, y: Int, on: Boolean) {}
+    override fun getSwitch(switch: Console.Switch) = false
 }
